ab1d00419a4a445ae60f1378ae0d60a165673f86
nightstardawn <nightstardawn@outlook.com>
2025-03-19 10:17:57 +0800

    chore：change package origin
    DetailInfo: 将本地的SDK来源修改为github中库

--- UnitySDK_2_3_1/com.yvr.utilities/Documentation~/CoreModules.md
+++
@@ -1,5 +0,0 @@
-# Core Modules
-
-这一部分会解释 YFramework中定义的主要模块，包括：
-
-1. [Log](./CoreModules/Log.md): 可拓展的 Log 模块
--- UnitySDK_2_3_1/com.yvr.utilities/Runtime/Package/PackageVersionObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-
-[System.Serializable]
-public class PackageVersionObject : ScriptableObject
-{
-    public List<PackageInfo> packages;
-}
